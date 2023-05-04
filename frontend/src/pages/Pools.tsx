931fab40fedd29acbaf1ae51ecda032f1cff7403
Jimmy <mimiouyang@hotmail.com>
2023-05-04 19:37:31 -0400

    Pools

---
+++ frontend/src/pages/Pools.tsx
@@ -0,0 +1,280 @@
+import { useEffect, useState } from "react"
+
+import InfoIcon from "@mui/icons-material/Info"
+import Box from "@mui/material/Box"
+import Grid from "@mui/material/Grid"
+import Paper from "@mui/material/Paper"
+import Slider from "@mui/material/Slider"
+import Tooltip from "@mui/material/Tooltip"
+import Typography from "@mui/material/Typography"
+
+import Bump from "../components/Bump"
+import Chord from "../components/Chord"
+import HeatMap from "../components/HeatMap"
+import MultiLine from "../components/MultiLine"
+import OptionsBar from "../components/OptionsBar"
+import Pie from "../components/Pie"
+import SimpleLine from "../components/SimpleLine"
+import { DailyStats } from "../interfaces/DailyStats"
+
+const api_url = import.meta.env.VITE_API_URL
+
+interface Data {
+	[key: string]: LineChartDataGroup
+}
+interface LineChartDataGroup {
+	[key: string]: LineChartData[]
+}
+interface LineChartData {
+	id: string
+	data: LineChartDataPoint[]
+}
+interface LineChartDataPoint {
+	x: string | number
+	y: number
+}
+
+const applyDateRange = (range: number[], lineData: Array<any>) => {
+	if (lineData.length == 0) return []
+	const start = Math.floor((range[0] / 100) * lineData.length)
+	const end = Math.floor((range[1] / 100) * lineData.length)
+	return lineData.slice(start, end)
+}
+
+function Pools() {
+	const [statType, setStatType] = useState("VOLUME" as keyof DailyStats)
+	const [data, setData] = useState<Data>({ thirty: { VOLUME: [] } })
+	const [wavax, setWavax] = useState<{ x: string; y: number }[]>([])
+	const [bump, setBump] = useState<Data>({ thirty: { VOLUME: [] } })
+	const [chord, setChord] = useState<{ labels: string[]; data: number[][] }>({
+		labels: [],
+		data: [],
+	})
+	const [pie, setPie] = useState<{
+		[keys: string]: { [keys: string]: { id: string; label: string; value: number }[] }
+	}>({
+		all_time: { SWAPS: [], USERS: [], VOLUME: [] },
+		thirty: { SWAPS: [], USERS: [], VOLUME: [] },
+		seven: { SWAPS: [], USERS: [], VOLUME: [] },
+	})
+	const [heat, setHeat] = useState<{ id: string; data: { x: string; y: number }[] }[]>([])
+	const [selectedData, setSelectedData] = useState<LineChartData[]>([
+		{
+			id: "dummy",
+			data: [{ x: "0", y: 0 }],
+		},
+	])
+	const [selectedBump, setSelectedBump] = useState<LineChartData[]>([
+		{
+			id: "dummy",
+			data: [{ x: 0, y: 1 }],
+		},
+	])
+	const [interval, setInterval] = useState("thirty")
+	const [range, setRange] = useState([0, 100])
+
+	useEffect(() => {
+		fetch(api_url + "/pool/daily_line")
+			.then((r) => r.json())
+			.then((d) => {
+				setData(d)
+				setSelectedData(d[interval][statType])
+			})
+		fetch(api_url + "/pool/daily_bump")
+			.then((r) => r.json())
+			.then((d) => {
+				setBump(d)
+				setSelectedBump(d[interval][statType])
+			})
+		fetch(api_url + "/pool/overlap")
+			.then((r) => r.json())
+			.then((d) => {
+				setChord(d["data"])
+			})
+
+		fetch(api_url + "/pool/pie")
+			.then((r) => r.json())
+			.then((d) => {
+				setPie(d)
+			})
+		fetch(api_url + "/pool/heat_map")
+			.then((r) => r.json())
+			.then((d) => {
+				setHeat(d["data"])
+			})
+		fetch(api_url + "/pool/wavax_line")
+			.then((r) => r.json())
+			.then((d) => {
+				setWavax(d["data"])
+			})
+	}, [])
+
+	useEffect(() => {
+		setSelectedData(data[interval][statType])
+		setSelectedBump(bump[interval][statType])
+	}, [statType, interval])
+
+	const handleStat = (_event: React.MouseEvent<HTMLElement>, newStat: string) => {
+		setStatType(newStat as keyof DailyStats)
+	}
+
+	const handleInterval = (_event: React.MouseEvent<HTMLElement>, newInterval: string) => {
+		setInterval(newInterval)
+	}
+
+	const handleRange = (_event: Event, newValue: number | number[]) => {
+		setRange(newValue as number[])
+	}
+
+	return (
+		<Grid container direction="column" spacing={2}>
+			<Grid item>
+				<Box
+					component={Paper}
+					display="flex"
+					justifyContent="space-between"
+					p={2}
+					sx={{ borderTop: 10, borderColor: "#ee5253" }}
+				>
+					<Typography variant="h3">Pools</Typography>
+				</Box>
+			</Grid>
+			<Grid item>
+				<OptionsBar
+					text={"Daily " + statType.charAt(0) + statType.slice(1).toLowerCase()}
+					statType={statType}
+					interval={interval}
+					handleStat={handleStat}
+					handleInterval={handleInterval}
+				/>
+			</Grid>
+			<Grid item>
+				<Box component={Paper} sx={{ height: "50vh" }}>
+					<MultiLine
+						data={selectedData}
+						interval={interval === "all_time" ? "month" : "day"}
+					/>
+				</Box>
+			</Grid>
+			<Grid item>
+				<OptionsBar
+					text={
+						"Daily Ranking by " + statType.charAt(0) + statType.slice(1).toLowerCase()
+					}
+					statType={statType}
+					interval={interval}
+					handleStat={handleStat}
+					handleInterval={handleInterval}
+				/>
+			</Grid>
+			<Grid item>
+				<Box component={Paper} sx={{ height: "60vh" }}>
+					<Bump data={selectedBump} />
+				</Box>
+				<Typography variant="subtitle1" sx={{ fontSize: 12 }}>
+					*Although it may appear that two lines are sharing the same rank on the earlier
+					dates, it is simply because of one of the lines is being extended to the
+					beginning. This is only a visual bump, not an actual conflict. Refer to the
+					points to see where each lines ranking properly starts.
+				</Typography>
+			</Grid>
+			<Grid item container>
+				<Grid item container direction="column" xs={6}>
+					<Grid item>
+						<OptionsBar
+							text={
+								statType.charAt(0) +
+								statType.slice(1).toLowerCase() +
+								" Distribution"
+							}
+							statType={statType}
+							interval={interval}
+							handleStat={handleStat}
+							handleInterval={handleInterval}
+						/>
+					</Grid>
+					<Grid item>
+						<Box component={Paper} sx={{ height: "60vh" }}>
+							<Pie data={pie} interval={interval} statType={statType} />
+							<Typography variant="subtitle2">
+								* distribution among top 10 pools
+							</Typography>
+						</Box>
+					</Grid>
+				</Grid>
+				<Grid item container direction="column" xs={6}>
+					<Grid item>
+						<Box
+							component={Paper}
+							display="flex"
+							justifyContent="space-between"
+							p={2.4}
+							sx={{ borderTop: 10, borderColor: "#ee5253" }}
+						>
+							<Typography variant="h4">User Overlap (7D)</Typography>
+							<Tooltip title="User overlap between the top 10 pools in the last 7 days.">
+								<InfoIcon />
+							</Tooltip>
+						</Box>
+					</Grid>
+					<Grid item>
+						<Box component={Paper} sx={{ height: "60vh" }}>
+							<Chord labels={chord.labels} data={chord.data} />
+						</Box>
+					</Grid>
+				</Grid>
+			</Grid>
+			<Grid item container direction="column" marginTop={2}>
+				<Grid item>
+					<Box
+						component={Paper}
+						display="flex"
+						justifyContent="space-between"
+						p={2.4}
+						sx={{ borderTop: 10, borderColor: "#ee5253" }}
+					>
+						<Typography variant="h4">Pool Volume Change by Platform (7D)</Typography>
+						<Tooltip title="Percent change in volume between the past week (the last 7 days) and the week before that. Considers the top 10 pools of the past week.">
+							<InfoIcon />
+						</Tooltip>
+					</Box>
+				</Grid>
+				<Grid item>
+					<Box component={Paper} sx={{ height: "60vh" }}>
+						<HeatMap data={heat} />
+					</Box>
+				</Grid>
+			</Grid>
+			<Grid item container direction="column">
+				<Grid item>
+					<Box
+						component={Paper}
+						display="flex"
+						justifyContent="space-between"
+						p={2.4}
+						sx={{ borderTop: 10, borderColor: "#ee5253" }}
+					>
+						<Typography variant="h4">WAVAX-X Pools Volume Percentage</Typography>
+						<Tooltip title="Percentage of volume going into WAVAX pairs. A chain's native token (or its wrapped version) is usually one of the top tokens to LP with.">
+							<InfoIcon />
+						</Tooltip>
+					</Box>
+				</Grid>
+				<Grid item>
+					<Box component={Paper} sx={{ height: "50vh" }}>
+						<SimpleLine data={applyDateRange(range, wavax)} type="Stable Percent" />
+					</Box>
+				</Grid>
+				<Grid item>
+					<Slider
+						value={range}
+						onChange={handleRange}
+						sx={{ color: "#ee5253", maxWidth: "95%" }}
+					/>
+				</Grid>
+			</Grid>
+		</Grid>
+	)
+}
+
+export default Pools
