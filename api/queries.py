16a509d0209ec8da56dcb0ee458446d2a9a368b1
Jimmy <mimiouyang@hotmail.com>
2023-05-04 02:53:53 -0400

    Removed unused

--- api/queries.py
+++ api/queries.py
@@ -1,39 +1,3 @@
-platform_recent_tx = """
-SELECT
-  block_timestamp AS date,
-  platform,
-  pool_name,
-  contract_address,
-  tx_hash,
-  symbol_in,
-  token_in,
-  amount_in,
-  symbol_out,
-  token_out,
-  amount_out
-FROM avalanche.core.ez_dex_swaps
-WHERE date >= DATEADD('day', -1, GETDATE())
-AND platform = '{}'
-ORDER BY date DESC
-LIMIT 1000;
-"""
-
-
-top_platforms = """
-SELECT
-  platform,
-  COUNT(DISTINCT tx_hash) AS tx_count,
-  COUNT(DISTINCT origin_from_address) AS users,
-  SUM(amount_in_usd) AS usd
-FROM
-  avalanche.core.ez_dex_swaps
-WHERE
-  block_timestamp >= DATEADD('day', -{}, GETDATE ())
-GROUP BY
-  platform
-"""
-
-
 general_recent_transactions = """
 SELECT
   block_timestamp AS date,
